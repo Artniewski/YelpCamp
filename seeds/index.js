1d9e9d72dd84b63aa268cf28d238f73a34c3f6b6
artniewski <artniewski@gmail.com>
2021-10-27 16:54:39 +0200

    Add Basic Styling
    Add: EjsMate engine, Bootstrap5;
    Create ejs boilerplate for each page and
    partials such as Navbar and Footer.
    Add image to campground model.
    Restyle index.ejs, show.ejs, new.ejs, edit.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aut corporis,' +
+                ' earum, et itaque iure modi nemo nostrum, odit omnis optio perspiciatis possimus rerum ' +
+                'ut veniam voluptatem voluptatum! Id, incidunt.',
+            price
         })
         await camp.save();
     }
